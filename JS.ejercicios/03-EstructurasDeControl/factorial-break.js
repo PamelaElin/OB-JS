0a48405bfd65476ef09578e74d28f3d2b61f337e
Pamela Elin <sosapamelaelin@gmail.com>
2022-08-09 12:18:29 -0300

    ejercicio03

---
+++ JS.ejercicios/03-EstructurasDeControl/factorial-break.js
@@ -0,0 +1,9 @@
+let i = 1;
+let j = 10;
+while (true) {
+    i *= j
+    j--
+    if (j === 1) 
+    break;
+}
+console.log(i) 
---
+++ JS.ejercicios/03-EstructurasDeControl/factorial-for.js
@@ -0,0 +1,12 @@
+// // solucion 1
+let factorial = 1;
+for (let i = 10; i >= 1; i--) {
+    factorial *= i
+}
+console.log(factorial)
+// solucion 2
+let num=10;
+for (let i = num-1; i>=1; i--) {
+    num*=i
+}
+console.log(num)
