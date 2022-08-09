79e37ea201b19c966a580a2314bf623355383944
Pamela Elin <sosapamelaelin@gmail.com>
2022-08-08 21:08:10 -0300

    sintaxis

---
+++ JS.ejercicios/02-sintaxis/sintaxis.js
@@ -0,0 +1,7 @@
+const lista=["Elin",29,true,new Date(1992,09,21),
+{titulo: "El principito",
+autor:"Antoine de Saint-Exupéry",
+fecha:new Date(1943,3, 1),
+url:"https://articulo.mercadolibre.com.ar/MLA-901711139-el-principito-libro-tapa-dura-ilustrado-original-blanco-_JM#position=1&search_layout=stack&type=pad&tracking_id=afb04c22-5190-4a95-8a13-960e9dd56728#position=1&search_layout=stack&type=pad&tracking_id=afb04c22-5190-4a95-8a13-960e9dd56728&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=1&ad_click_id=M2Q5YWI5ZDUtNTc3NS00Y2M1LWFkZDItYWVlNGUzZDRmYjZm"},]; 
+
+console.log(lista);
