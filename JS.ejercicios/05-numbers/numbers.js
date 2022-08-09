3dd67c40272310a15af8b510e027c6b1f6cca753
Pamela Elin <sosapamelaelin@gmail.com>
2022-08-09 18:05:11 -0300

    05

---
+++ JS.ejercicios/05-numbers/numbers.js
@@ -0,0 +1,6 @@
+const altura_cm = 151;
+const altura_m = 1.51;
+const peso_kg = 54.2;
+const altura_red = Math.ceil(altura_m);
+const peso_red = Math.floor(peso_kg);
+const sonIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
