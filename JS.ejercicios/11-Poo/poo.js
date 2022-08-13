97ab23f31aed4d16f58f892c38ad0b3b2b02e8e7
Pamela Elin <sosapamelaelin@gmail.com>
2022-08-13 11:39:17 -0300

    11

---
+++ JS.ejercicios/11-Poo/poo.js
@@ -0,0 +1,17 @@
+class Estudiante {
+  nombre = "Elin";
+  asignaturas = ["Javascript", "HTML", "CSS"];
+
+  obtenDatos() {
+    return {
+      nombre: this.nombre,
+      asignaturas: this.asignaturas,
+    };
+  }
+}
+
+const estudiante = new Estudiante();
+
+console.log(estudiante.obtenDatos());
+
+
