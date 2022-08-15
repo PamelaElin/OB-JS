12cc249c1609f9943022bbf46967a6b672c98444
Pamela Elin <sosapamelaelin@gmail.com>
2022-08-15 12:52:53 -0300

    14

---
+++ JS.ejercicios/14-gestionDeEventos/index.js
@@ -0,0 +1,8 @@
+const btn=document.querySelector("button")
+btn.addEventListener("click",()=>alert("click en el botón")
+)
+
+
+$("button").click(()=> {
+    console.log("Hola, estoy utilizando jQuery")
+})
