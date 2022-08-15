935d4e23869f0b092ffec5a4d8003f3dc06ef956
Pamela Elin <sosapamelaelin@gmail.com>
2022-08-15 20:16:15 -0300

    15

---
+++ JS.ejercicios/15-persistenciaDeDatos/index.js
@@ -0,0 +1,12 @@
+const nombre="Elin";
+const apellido="Sosa";
+const persona={
+   nombre,
+    apellido
+}
+//  sessionStorage.setItem("persona",JSON.stringify(persona))
+//  localStorage.setItem("persona",JSON.stringify(persona));
+// const time=new Date()
+
+// console.log(document.cookie=`persona=${JSON.stringify(persona)};expires=${new Date(time.getTime() + 2 * 60000)}`)
+
